4e28b192efa05c61d6d4dfedc6980ba489435e0b
SaraaX404 <thathsarasenanayaka74@gmail.com>
2023-09-18 17:24:08 +0530

    feat:SessionAuth

--- src/auth/local.strategy.ts
+++ src/auth/local.strategy.ts
@@ -11,6 +11,7 @@ export class LocalStrategy extends PassportStrategy(Strategy){
     }
 
 
+
     async validate(username:string, password:string){
         console.log(username)
         const user = await this.authService.validatePassword(username, password)
